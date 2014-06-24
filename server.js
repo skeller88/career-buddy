0aa443e2cedf259da89783539752be3b5203c6cf
skeller88 <skeller88@gmail.com>
2014-06-23 17:40:49 -0700

    Remove unneeded files from package.json

--- server.js
+++ server.js
@@ -1,10 +1,74 @@
+var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+
+// var pg = require('pg').native;
+
+// var client = new pg.Client({
+//   host: 'ec2-54-243-49-204.compute-1.amazonaws.com',
+//   port: '5432',
+//   database: 'da8g81b6klacig',
+//   user: 'gbucxcgyytxjfu',
+//   password: 'EJ8f0r_idtBFxhXqqz8kQ814SX'
+// });
+
+// var knex = require('knex')({
+//   client: 'pg',
+//   connection: "dbname=d79onnfkt300fv host=ec2-54-204-32-91.compute-1.amazonaws.com port=5432 user=jitpluhwkhnmfi password=SFqDPMve7-IXcGZij-bY6WeoK2 sslmode=require"
+// });
+
+// var knex = require('knex')({
+//   host: 'ec2-54-243-49-204.compute-1.amazonaws.com',
+//   port: '5432',
+//   database: 'da8g81b6klacig',
+//   user: 'gbucxcgyytxjfu',
+//   password: 'EJ8f0r_idtBFxhXqqz8kQ814SX'
+// });
+
 var app = express();
 
-require('./server/request-handler')(app);
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.urlencoded());
+app.use(express.static(__dirname + '/app'));
 
-var port = process.env.PORT || 3000;
+app.get('/', function(req,res){
+  // check to see if there's a user id parameter
+  // if there is, run a query through the DB to see if that user id exists; return with data if it does
+  // send res.redirect to '/new with information'
 
-app.listen(port);
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    console.log('err: ', err);
+    var query = client.query('SELECT * FROM careers limit 5');
+    query.on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+  });
+  console.log('get');
 
-console.log('Server now listening on port ' + port);
+  res.send('/index.html');
+});
+
+function serveIndex(req, res) {
+  statusCode = 200;
+
+  // client.connect();
+
+  // knex.select('*').table('careers').limit(2)
+  //   .then(function(rows) {
+  //     console.log('rows', rows);
+  //   });
+
+  pg.connect(database_url, function(err, client) {
+    var query = client.query('SELECT * FROM careers');
+
+    query.on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+  });
+};
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Server now listening on port ' + app.get('port'));
+});
+
