6e3d64bc7db1c1ba43611152c90142e8b569012d
skeller88 <skeller88@gmail.com>
2014-07-19 12:18:57 -0700

    Tool tip works properly

--- app/js/directives.js
+++ app/js/directives.js
@@ -19,23 +19,13 @@ angular.module('myApp.directives', ['kendo.directives']).
           function drawChart() {
               if(d3.select('svg')) d3.select('svg').remove();
 
-              console.log($window.innerHeight);
-              console.log($('.sk-multi-select-container').css('height'));
-              console.log($('.sk-career-selector').height());
-              console.log($('.sk-navbar-background').css('height'));
+              /* DIMENSIONS */
 
-              scope.blah = function() {
-                  console.log('blah');
-              }
-
-              //svg dimensions
-              /* .sk-career-selector has a total height of 172px */
-              // var sh = $window.innerHeight - 210 - $('.sk-navbar-background').height();
-              // var sh = $window.innerHeight - $('.sk-career-selector').height() - $('.sk-navbar-background').height();
+              //svg
               var sh = element.height();
               var sw = element.width();
 
-              //chart dimensions
+              //chart
               var margin = {top: 15, right: 15, bottom: 15, left: 15};
               var padding = {top: 0, right: 0, bottom: 40, left: 50};
 
@@ -44,6 +34,13 @@ angular.module('myApp.directives', ['kendo.directives']).
               var iw = w - padding.left - padding.right;
               var ih = h - padding.top - padding.bottom;
 
+              //bubbles
+              var rmin = 4;
+              var rmax = 15;
+
+              //tooltip height
+              var tth = 28;
+
               //Margin between labels and chart axes
               var yLabelP = 45;
 
@@ -51,24 +48,13 @@ angular.module('myApp.directives', ['kendo.directives']).
               var xLegendP = 15;
               var yLegendP = 15;
 
-              //sizes of bubbles 
-              var rmin = 4;
-              var rmax = 15;
-
-              var svg = d3.select(element[0])
-                .append('svg')
-                .attr('class', 'sk-chart-svg');
-              
-              var outerChart = svg.append('g')
-                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-              var innerChart = outerChart.append('g')
-                .attr('transform', 'translate(' + padding.left + ',' + padding.top + ')');
 
               var xlabel = "Projected Job Growth: 2012-2022 (%)";
               var ylabel = "Median Annual Salary: 2012 ($1000s)";
               var rlabel = "Number Employeed: 2012 (1k)";
               var legendText = "Legend";
+
+              /* SCALES */
 
               //career_percent_emp_change in %
               var xScale = d3.scale.linear()
@@ -92,7 +78,37 @@ angular.module('myApp.directives', ['kendo.directives']).
               var yAxis = d3.svg.axis()
                 .scale(yScale)
                 .orient('left');
-                
+
+              /* DRAW CHART */
+
+              //tooltip
+              // var tooltip = d3.select('body')
+              //   .append('div')
+              //   .attr('class', 'sk-tooltip')
+              //   .style('opacity', 1);
+              var tip = d3.tip()
+                  .attr('class', 'sk-tooltip')
+                  .offset([-15, -13])
+                  .html(function(d) {
+                    return '<div class="sk-career-name">' + d.career_name + '</div>' +
+                        '<div class="sk-emp-change">Expected 10-year Growth: ' + d.career_percent_emp_change + '%</div>' +
+                        '<div class="sk-ann-wage">Annual wage: $' + d.career_med_ann_wage + '</div>' +
+                        '<div class="sk-ann-wage">People employed: ' + d.career_2012_emp + '</div>'
+                  })
+
+              var svg = d3.select(element[0])
+                .append('svg')
+                .attr('class', 'sk-chart-svg');
+
+              svg.call(tip);
+              
+              var outerChart = svg.append('g')
+                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+              var innerChart = outerChart.append('g')
+                .attr('class', 'innerChart')
+                .attr('transform', 'translate(' + padding.left + ',' + padding.top + ')');
+
               //axes
               innerChart.append('g')
                 .attr('class', 'axis')
@@ -117,7 +133,7 @@ angular.module('myApp.directives', ['kendo.directives']).
                 .style('text-anchor', 'middle')
                 .text(ylabel);
 
-              innerChart.append('div')
+              var legend = innerChart.append('div')
                 .attr('class', 'sk-legend')
                 .attr('ng-click', 'blah')
                 .attr('transform', 'translate(' + xLegendP + ',' + (ih - yLegendP) + ')')
@@ -133,7 +149,28 @@ angular.module('myApp.directives', ['kendo.directives']).
                   .attr('class', 'sk-data-point')
                   .attr('cx', function(d) { return xScale(d.career_percent_emp_change); })
                   .attr('cy', function(d) { return yScale(d.career_med_ann_wage/1000); })
-                  .attr('r', function(d) { return rScale(d.career_2012_emp); });
+                  .attr('r', function(d) { return rScale(d.career_2012_emp); })
+                  // .on('mouseover', function(){ console.log('over');})
+                  // .on('mouseout', function(){ console.log('out');})
+                  // .on('mouseenter', function(d) {
+                  //   console.log('enter');
+                  //     tooltip.transition()
+                  //         .duration(500)  
+                  //         .style("opacity", 0);
+                  //     tooltip.transition()
+                  //         .duration(200)
+                  //         .style('opacity', 0.9);
+                  //     tooltip.html(
+                  //       '<span class="sk-career-name">' + d.career_name + '</span>' +
+                  //       '<span class="sk-emp-change">Growth: ' + d.career_percent_emp_change + '</span>' +
+                  //       '<span class="sk-ann-wage">Annual Wage: ' + d.career_med_ann_wage + '</span>' +
+                  //       '<span class="sk-ann-wage">People employed: ' + d.career_2012_emp + '</span>'
+                  //       )
+                  //       .style("left", (d3.event.pageX) + "px")      
+                  //       .style("top", (d3.event.pageY - tth) + "px");
+                  // });
+                  .on('mouseover', tip.show)
+                  .on('mouseout', tip.hide);
 
                 circles
                   .enter()
