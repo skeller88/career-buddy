ebf71eccd257d1522ffb29b3bcaa57767d2ce4ea
skeller88 <skeller88@gmail.com>
2014-07-26 18:00:46 -0700

    Refactor tests to use mocha and chai Chai allows for custom error messages. Jasmine-node does not.

--- spec/dbHelpersSpec.js
+++ spec/dbHelpersSpec.js
@@ -1,9 +1,10 @@
+var assert = require('assert');
 var dbHelpers = require('../src/util/dbHelpers.js');
 
 describe('dbHelpers.queryCareerNames', function() {
     it('retrieves all career names from the database', function(done) {
         dbHelpers.queryCareerNames().then(function(careerNames) {
-            expect(careerNames.length).toBe(1074);
+            assert.equal(careerNames.length, 1074, 'query did not select correct number of careers');
             done();
         });
     });
@@ -13,8 +14,8 @@ describe('dbHelpers.queryCareerData', function() {
     it('retrieves all data for given career names', function(done) {
         dbHelpers.queryCareerData(['Mathematicians', 'Nurse practitioners', 'Physician assistants']).then(function(matchedCareers) {
             var careerKeys = Object.keys(matchedCareers[0]);
-            expect(careerKeys.length).toBe(12);
-            expect(matchedCareers.length).toBe(3);
+            assert.equal(careerKeys.length, 12, 'query did not select correct number of columns');
+            assert.equal(matchedCareers.length, 3, 'query did not select correct number of careers');
             done();
         });
     });
