9acb29fed9df65d32ca355f0a50e71d9b23c93bc
skeller88 <skeller88@gmail.com>
2014-08-06 15:11:09 -0700

    K-means cluster analysis. Still need to normalize data prior to running the analysis.

--- data-manipulation/dataAnalysis.r
+++ data-manipulation/dataAnalysis.r
@@ -1,2 +1,54 @@
-oes <- read.csv("/Users/Shane/career-buddy-data/ep-tables-for-csv.csv")
-mean(oes[,3])
+#all career data
+
+ac <- read.csv("/Users/Shane/Documents/SugarSync/career-buddy/data-manipulation/careers_maroon.csv")
+
+#descriptive statistics
+ac[0,]
+density(ac$career_med_ann_wage)
+hist(ac$career_num_emp_change)
+hist(ac$career_percent_emp_change)
+hist(ac$career_job_openings)
+
+
+#select all continuous variables
+ac <- as.data.frame(ac)
+c <- ac[,c('career_med_ann_wage', 'career_num_emp_change', 'career_percent_emp_change', 'career_job_openings')]
+ac$career_job_openings[1:10]
+
+
+#problems 
+#- analyze only certain columns in data, and eliminate rows with NA, but after clustering is done add
+# a new column to the original csv file containing the cluster number each row belongs to
+# with the rows matching up 
+c[is.na(c),]
+
+#cleaned career data with all rows with 'NA' removed
+# cc <- c[complete.cases(c),]
+cc <- na.omit(c)
+#reassign row numbers to avoid confusion caused by missing rows
+rownames(cc) <- 1:nrow(cc)
+
+#generate SSE scree plot to determine number of k-means clusters: http://stackoverflow.com/questions/15376075/cluster-analysis-in-r-determine-the-optimal-number-of-clusters
+wssplot <- function(data) {
+  wss <- (nrow(data)-1)*sum(apply(data,2,var))
+  max_cluster <- (nrow(data) - 1)
+  for (i in 2:max_cluster) 
+    wss[i] <- sum(kmeans(data, centers=i, nstart=25)$withinss)
+  
+    plot(1:max_cluster, wss, type="b", xlab="Number of Clusters",
+       ylab="Within groups sum of squares")
+}
+
+wssplot(dfc)
+#4 appears to be optimal 
+
+# K-Means Cluster Analysis
+fit <- kmeans(cc, 6) # 6 cluster solution
+# get cluster means 
+# aggregate(cc,by=list(fit$cluster),FUN=mean)
+# append cluster assignment
+# cc <- data.frame(cc, fit$cluster)
+
+#plot results
+plot(cc$height, dfc$salary, type="p", xlab = "height", ylab="salary", col=dfc$color)
+
--- data-manipulation/descriptive-stats.r
+++ data-manipulation/descriptive-stats.r
@@ -1,3 +1,23 @@
+ac <- read.csv("/Users/Shane/Documents/SugarSync/career-buddy/data-manipulation/careers_maroon.csv")
+
+#select two columns
+ac <- as.data.frame(ac)
+cnames <- ac[0,]
+#  [1] career_name               career_occ_code          
+#  [3] career_2012_emp           career_2022_emp          
+#  [5] career_num_emp_change     career_percent_emp_change
+#  [7] career_percent_self_emp   career_job_openings      
+#  [9] career_med_ann_wage       career_edu               
+# [11] career_work_exp           career_job_training 
+min(ac[['career_num_emp_change']])
+mean(ac[['career_num_emp_change']])
+max(ac[['career_num_emp_change']])
+
+min(ac[['career_percent_emp_change']])
+mean(ac[['career_percent_emp_change']])
+max(ac[['career_percent_emp_change']])
+
+#Didn't use any of this code. Attempt at data manipulation using R. 
 #change blank values to NA, and colnames to same names as database in case manipulation is needed
 d <- read.csv('/Users/Shane/career-buddy-data/ep-tables-for-csv.csv', na.strings=c(' ', ''))
 library(sqldf)
